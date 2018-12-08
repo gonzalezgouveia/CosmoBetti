c95f41237f9f47918a06429e91388a64f5415319
Rafa <gonzalez.gouveia@gmail.com>
2018-12-08 11:33:33 +0100

    add docs for main scripts
    - add Rd files for betti.fun and bettimatrix
    - erase unnecesary R files

--- R/bettivector.R
+++ R/bettivector.R
@@ -8,7 +8,7 @@ library(tidyverse)
 
 #' compute the difference between birth-death at time t from a matrix.
 #' internal function used in bettimatrix
-#' 
+#'
 #' @param bcmat is a ?times? matrix
 #' @param t is the time or distance to compute betti.fun
 #' @return the number of birth - death
@@ -18,16 +18,18 @@ betti.fun <- function(bcmat,t){
   return(nb-nd)
 }
 
-#' compute betti curves until some order from persistence diagram
+#' Compute Betti Curves
+#'
+#' Compute Betti Curves until some order from persistence diagram
 #' if order=-1 then the order is infered from diag
 #' if xmax=-1 the max lenght is infered from diag
-#' 
+#'
 #' @param diag is a persistence diagram from TDA o TDAstats
 #' @param order is the maximum order the betti curve is computed
 #' @param xmax is the maximum length/time the betti curve is computed
 #' @param l is the dimension of each betticurve
 #' @return a 3 times l matrix with the betticurve
-#' @examples 
+#' @examples
 #' library(TDA)
 #' data <- cbind(runif(10), runif(10))
 #' alpha_complex <- alphaComplexDiag(data)
@@ -43,23 +45,23 @@ bettimatrix <- function(diag, order = -1, xmax=-1, l = 100){
     print('diag not a valid persistence matrix')
     break
   }
-  
+
   # assign xmax automatically, info in description
   if (xmax == -1){
     xmax <- max(diag[-1,2:3]) * 1.05 # maximum plus 5%
   }
-  
+
   # assign order automatically, info in description
   if (order == -1){
     order <- max(alpha_diag[,1]) # starts at 0
   }
-  
+
   # create the x dimension for the bettimatrix
   xseq <- seq((xmax/(2*l)), xmax, length.out = l)
-  
+
   bettimatrix <- matrix(NA, ncol = 3, nrow = 0)
   colnames(bettimatrix) <- c('order', 'xseq', 'value')
-  
+
   for(i in 0:order){
     if(order==0){
       phx <- diag[which(diag[,1]==i), 2:3][-1,]
@@ -68,7 +70,7 @@ bettimatrix <- function(diag, order = -1, xmax=-1, l = 100){
     }
     for(j in 1:l){
       bettivalue <- betti.fun(phx, xseq[j])
-      bettimatrix <- rbind(bettimatrix, 
+      bettimatrix <- rbind(bettimatrix,
                            c(i, xseq[j], bettivalue))
     }
   }
--- R/por revisar/clasificacion/resultados.R
+++
@@ -1,8 +0,0 @@
-#resultados experimento de clasificacion
-errores <- c(27.2,23.6,11.2)
-metodos <- c("LDA","SVM","Bubenik")
-plot(errores,xaxt="n",type="b",xlim=c(0.5,3.5),ylim=c(5,30),
-     ylab="porcentaje",xlab="",main="Error de clasificacion")
-axis(1,1:3,labels = F)
-text(1:3+0.1, par("usr")[3] - 3, labels = as.vector(metodos), 
-     srt = 50, pos = 2, xpd = T,cex=1)
