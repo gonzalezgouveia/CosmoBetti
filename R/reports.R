cc91d434ea7540c7a7b6f05475511f11be3e9574
Rafa <gonzalez.gouveia@gmail.com>
2018-12-10 22:25:32 +0100

    naming and documenting
    - renaming functions and variables names
    - refactoring documentation

---
+++ R/reports.R
@@ -0,0 +1,15 @@
+# graphical reports and aggregate tables
+
+#' Plot Betti Curves
+#'
+#' Plot Betti Curves from persistence diagram of desired dimension
+#' in a ggplot style with facets and free scales
+#'
+#' @param betticurve is a betti matrix from bettimatrix
+#' @examples
+#' plot_betti(betticurve)
+plot_betti_curve <- function(betticurve){
+  ggplot2::ggplot(betticurve, ggplot2::aes(x = radius, y = value, color = dim)) +
+    ggplot2::geom_line() +
+    ggplot2::facet_grid(rows = ggplot2::vars(dim), scales = "free")
+}
